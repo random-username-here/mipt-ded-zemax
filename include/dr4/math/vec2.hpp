2555f0fbe6af07b0d1d6eaa1fc1872983f6c609b
kzueirf12345 <jaba0192837465@gmail.com>
2025-11-02 22:14:48 +0300

    expand Vec2f operators

--- include/dr4/math/vec2.hpp
+++ include/dr4/math/vec2.hpp
@@ -13,14 +13,23 @@ struct Vec2f {
     Vec2f(float x_, float y_)
         :x(x_), y(y_) {}
 
-    inline Vec2f operator-() { return Vec2f(-x, -y); }
-    inline Vec2f operator+(Vec2f other) { return Vec2f(x + other.x, y + other.y); }
-    inline Vec2f operator-(Vec2f other) { return Vec2f(x - other.x, y - other.y); }
-    inline Vec2f operator*(float k) { return Vec2f(x * k, y * k); }
-    inline Vec2f operator/(float k) { return Vec2f(x / k, y / k); }
+    inline Vec2f operator-() const { return Vec2f(-x, -y); }
+    inline Vec2f operator+(Vec2f other) const { return Vec2f(x + other.x, y + other.y); }
+    inline Vec2f operator-(Vec2f other) const { return Vec2f(x - other.x, y - other.y); }
+    inline Vec2f operator*(float k) const { return Vec2f(x * k, y * k); }
+    inline Vec2f operator/(float k) const { return Vec2f(x / k, y / k); }
+
+    inline Vec2f& operator+=(Vec2f other) { x += other.x; y += other.y; return *this; }
+    inline Vec2f& operator-=(Vec2f other) { x -= other.x; y -= other.y; return *this; }
+    inline Vec2f& operator*=(float k) { x *= k; y *= k; return *this; }
+    inline Vec2f& operator/=(float k) { x /= k; y /= k; return *this; }
 
     Vec2f Clamped(Vec2f min, Vec2f max) const;
 };
+
+inline Vec2f operator*(float k, const Vec2f& vec) {
+    return Vec2f(vec.x * k, vec.y * k);
+}
 
 }; // namespace dr4
 
