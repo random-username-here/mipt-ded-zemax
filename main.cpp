4257061d9cb29b554bf67b4504a327a4e38eedba
Abrutin Ivan Dmitrievich <mai.abrutinid@gmail.com>
2025-11-09 22:15:17 +0300

    add widget iterator

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <memory>
+#include <vector>
+#include <cstddef>
+#include <iostream>
+
+#include "hui/widget.hpp"
+
+int main() {
+    hui::Widget *widget = new hui::Widget({}, nullptr, nullptr);
+}
---
+++ run.sh
@@ -0,0 +1,3 @@
+mkdir -p build
+g++ -Iinclude -std=c++17 main.cpp -o build/main.out
+./build/main.out
