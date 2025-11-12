287d662e0834a9938d41166cb1444be214de861d
sevsol <sevsol2512@gmail.com>
2025-11-12 11:27:15 +0300

    Added default colors

--- include/dr4/math/color.hpp
+++ include/dr4/math/color.hpp
@@ -13,10 +13,25 @@ struct Color {
 
     Color(uint8_t r_, uint8_t g_, uint8_t b_, uint8_t a_=ColorMaxValue)
         :r(r_), g(g_), b(b_), a(a_) {}
-    
+
     Color()
         :r(0), g(0), b(0), a(ColorMaxValue) {}
+
+    static const Color White;
+    static const Color Black;
+    static const Color Red;
+    static const Color Green;
+    static const Color Blue;
+    static const Color Transparent;
 };
+
+inline const Color Color::Transparent = {0, 0, 0, 0};
+
+inline const Color Color::White = { 255, 255, 255 };
+inline const Color Color::Black = { 0,     0,   0 };
+inline const Color Color::Red   = { 255,   0,   0 };
+inline const Color Color::Green = { 0,   255,   0 };
+inline const Color Color::Blue  = { 0,     0, 255 };
 
 }; // namespace dr4
 
